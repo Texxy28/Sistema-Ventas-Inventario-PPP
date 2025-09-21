d828312c40a8d4ecf3bfb92ca4edae6f2ca8707b
Texx28 <willyortizlopezaeiou@gmail.com>
2025-09-21 12:32:19 -0500

    Desarrollo del CRUD de productos (crear, editar, eliminar, consultar).

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { Pool } from 'pg';
+import ProductRouter from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -11,17 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-pool.connect()
-  .then(client => {
-    console.log('Conectado a la base de datos');
-    client.release();
-  })
-  .catch(err => console.error('Error de conexión a BD:', err));
+app.use("/api/product", ProductRouter)
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
